37ad0131a68701c9d1d5004445fd7ed15bc5a159
rm-reins <rm_reins@icloud.com>
2025-07-02 17:13:53 +0200

    feat: add IconButton component and screens for adding and listing places

---
+++ src/components/ui/IconButton.tsx
@@ -0,0 +1,34 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+interface IconButtonProps {
+  icon: keyof typeof Ionicons.glyphMap;
+  size: number;
+  color: string | undefined;
+  onPress: () => void;
+}
+
+function IconButton({ icon, size, color, onPress }: IconButtonProps) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
