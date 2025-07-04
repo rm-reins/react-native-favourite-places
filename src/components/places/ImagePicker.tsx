5f5668bcec4b56ddfa995c78e8decd806f8100a4
rm-reins <rm_reins@icloud.com>
2025-07-04 02:21:53 +0200

    chore: cleanup

--- src/components/places/ImagePicker.tsx
+++ src/components/places/ImagePicker.tsx
@@ -1,4 +1,4 @@
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { useState } from "react";
 import {
   launchCameraAsync,
