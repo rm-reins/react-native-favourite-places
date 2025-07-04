d6c424f2462b3ee3e65ad9f8695c35761bc705b8
rm-reins <rm_reins@icloud.com>
2025-07-04 02:20:56 +0200

    feat: add dotenv config

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ["dist/*"],
+    rules: {
+      // Allow @env imports
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["@env"],
+        },
+      ],
+    },
+  },
+]);
