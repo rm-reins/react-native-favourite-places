d2ec7ff57845e9fed6d76ee63cdbb0d1b24346d8
rm-reins <rm_reins@icloud.com>
2025-07-04 17:53:32 +0200

    feat: added confirmation for picked location

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { MapPressEvent, Marker } from "react-native-maps";
+import { NavigationProp } from "@/types/navigation";
+import IconButton from "@/components/ui/IconButton";
 
-function Map() {
+interface MapProps {
+  navigation: NavigationProp;
+}
+
+function Map({ navigation }: MapProps) {
   const [selectedLocation, setSelectedLocation] = useState({
     lat: 0.0,
     lng: 0.0,
@@ -21,6 +27,31 @@ function Map() {
 
     setSelectedLocation({ lat: lat, lng: lng });
   }
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert("No location picked", "You have to pick a loction first!");
+      return;
+    }
+
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={20}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
 
   return (
     <MapView
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,7 +2,12 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
   AllPlaces: undefined;
-  AddPlace: undefined;
+  AddPlace:
+    | {
+        pickedLat: number;
+        pickedLng: number;
+      }
+    | undefined;
   Map: undefined;
 };
 
