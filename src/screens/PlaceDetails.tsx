794e18c8777d4412b06d7625cd72bc10edff1623
rm-reins <rm_reins@icloud.com>
2025-07-06 04:22:30 +0200

    feat: create screens index and implement PlaceDetails component

--- src/screens/PlaceDetails.tsx
+++ src/screens/PlaceDetails.tsx
@@ -1,3 +1,61 @@
-function PlacesDetails() {}
+import { OutlinedButton } from "@/components/ui";
+import { Colours } from "@/constants/colors";
+import { RootStackParamList } from "@/types/navigation";
+import { RouteProp } from "@react-navigation/native";
+import { useEffect } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+
+type PlaceDetailsRouteProp = RouteProp<RootStackParamList, "PlaceDetails">;
+
+interface PlaceDetailsProps {
+  route: PlaceDetailsRouteProp;
+}
+
+function PlacesDetails({ route }: PlaceDetailsProps) {
+  function showOnMapHandler() {}
+
+  const selectedPlaceId = route.params.placeId;
+
+  useEffect(() => {}, [selectedPlaceId]);
+
+  return (
+    <ScrollView>
+      <Image style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>ADDRESS</Text>
+        </View>
+
+        <OutlinedButton
+          icon="map"
+          onPress={showOnMapHandler}
+        >
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
+  );
+}
 
 export default PlacesDetails;
+
+const styles = StyleSheet.create({
+  image: {
+    height: "35%",
+    minHeight: 300,
+    width: "100%",
+  },
+  locationContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: Colours.primary500,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+import AddPlace from "./AddPlace";
+import AllPlaces from "./AllPlaces";
+import Map from "./Map";
+import PlacesDetails from "./PlaceDetails";
+
+export { AddPlace, AllPlaces, Map, PlacesDetails };
